7c35dd490e4ca6b0bf89c70f81a914c07c8765c8
kevin36936 <kevin36936@gmail.com>
2026-02-11 15:15:25 +0800

    feat: implement cart state management with add/remove functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,73 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import CartItem from "./components/CartItem"
-interface Item {
-  id: number;
-  title: string;
-  price: number;
-  image: string
-}
-function App() {
-  const [items, setItems] = useState<Item[]>([]);
-  const [loading, setLoading] = useState<boolean>(true)
+import type { Item, CartItem } from "./type";
 
-  const total_in_cent = items.reduce((sum, item) => sum + item.price * 100, 0)
-  const total_in_dollar = (total_in_cent / 100).toFixed(2)
+function App() {
+  const [products, setProducts] = useState<Item[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const total_in_cent = products.reduce(
+    (sum, item) => sum + item.price * 100,
+    0,
+  );
+  const total_in_dollar = (total_in_cent / 100).toFixed(2);
   useEffect(() => {
     setLoading(true);
-    fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(data => {
-      setItems(data);
-      setLoading(false)
-    })
-  }, [])
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      });
+  }, []);
 
-  if(loading) {
-    return <div>Loading...</div>
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
-  const removeItem = (id: number) => {
-    setItems((prev) => prev.filter((i) => i.id !== id));
-  }
+  const addToCart = (product: Item) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find(({ id }) => id === product.id);
+      if (!existingItem) {
+        const newCartItem: CartItem = {
+          ...product,
+          quantity: 1,
+        };
+        return [...prevCart, newCartItem];
+      } else {
+        const updatedCart = prevCart.map((item) =>
+          (item.id === product.id)
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+        return updatedCart;
+      }
+    });
+  };
+
+  const removeFromCart = (id: number) => {
+    setCart(prev => prev.filter((item) => item.id !== id));
+  };
+
+
   return (
     <>
       <h1>My Shopping Cart</h1>
-      <p>Items in Cart: {items.length}</p>
+      <p>Items in Cart: {products.length}</p>
       <p>Total price: ${total_in_dollar}</p>
       <div>
         Items:{" "}
-        {items.map((item) => (
-          <CartItem 
-          key={item.id}
-          id={item.id}
-           name={item.title}
+        {products.map((item) => (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            name={item.title}
             price={item.price}
             image={item.image}
-            onRemove={removeItem} />
+            onRemove={removeItem}
+          />
         ))}
       </div>
     </>

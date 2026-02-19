f830bbffacc93e8d15a3f9689ddd67547b38085e
kevin36936 <kevinlai36936@gmail.com>
2026-02-19 13:43:36 +0800

    initialize server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,26 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express"
+import cors from "cors"
 
-const app = express()
+const app = express();
+
 app.use(cors());
 app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({message: "Shopping Cart API is running"});
+})
+
+
+app.get("/api/test", (req, res) => {
+    res.json({
+        message: "Hello from backend!",
+        timestamp: new Date().toISOString(),
+        status: "success"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
